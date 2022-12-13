8fc1d61440e1c58371a0e2a1f611fe04f3b8ba40
tanyongrw <rudjaya22934@gmail.com>
2022-12-13 17:18:51 +0700

    finishlab_4.cpp

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -9,7 +9,20 @@ int main(){
 	int id[] = {123,196,225,329,420,451,515,666,690,787};
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
 
-    //Write your code here.
+    int i = 0;
+    double num;
+    cout << "Please input GPA: " ;
+    cin >> num ;
+    while (i < 10)
+    {
+    if (gpa[i] >= num)
+    {
+    cout << id[i] << " " ;
+    cout << name[i] << " ";
+    cout << gpa[i] << "\n" ;
+    }
+    i++;
+    }
 		
 	return 0;
 }

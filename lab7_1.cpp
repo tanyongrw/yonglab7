88c396a30dc875685d7b4988a5a2839b42ee369d
tanyongrw <rudjaya22934@gmail.com>
2022-12-13 16:31:13 +0700

    finishlab7_1.cpp

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -33,9 +33,24 @@ string func3(string x){
 	return y;	
 }
 
-int main(){
-    cout >> "Input text: "
-    cout >> "Reversed text: "
-    cout >> "Palindrome: "
+int main()
+{
+    string e , f , g , h ;
+	cout << "Input text: " ;
+	cin >> e ;
+	f = func3(e) ;
+    cout << "Reversed text: " ;
+	g = func1(e) ;
+	h = func1(f) ;
+	cout << g << "\n" ;
+    cout << "Palindrome: " ;
+	if(f==h)
+	{
+		cout << "Yes" << "\n" ;
+	}
+	else
+	{
+		cout << "No" << "\n" ;
+	}
     return 0;
 }
